6142dae4ebae1cfc06b1d22b1ba2a8e757f8c944
Ömer Faruk Gündoğdu <omerrfarukgundogdu@gmail.com>
2025-03-12 14:43:51 +0300

    1 - 14 arasındaki ders içeriği

---
+++ ApiProjeKampi.WebApi/Controllers/ChefsController.cs
@@ -0,0 +1,55 @@
+﻿using ApiProjeKampi.WebApi.Context;
+using ApiProjeKampi.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChefsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ChefsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult ChefList()
+        {
+            var values = _context.Chefs.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateChef(Chef chef) {
+            _context.Chefs.Add(chef);
+            _context.SaveChanges();
+            return Ok("Şef Sisteme Başarıyla Eklendi.");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteChef(int id) {
+            var value = _context.Chefs.Find(id);
+            _context.Chefs.Remove(value);
+            _context.SaveChanges();
+            return Ok("Şef Sistemden Başarıyla Silindi.");
+        }
+
+        [HttpGet("GetChef")]
+        public IActionResult GetChef(int id) {
+            var value = _context.Chefs.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateChef(Chef chef)
+        {
+            _context.Chefs.Update(chef);
+            _context.SaveChanges();
+            return Ok("Şef Bilgileri Sistemden Başarıyla Güncellendi.");
+        }
+    }
+}
---
+++ ApiProjeKampi.WebApi/Migrations/20250312103551_mig2.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiProjeKampi.WebApi.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    ContactId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MapLocation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OpenHours = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.ContactId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
