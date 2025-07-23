71ce56aff8c880ffed318f8792516f5d6c2427d2
Ömer Faruk Gündoğdu <omerrfarukgundogdu@gmail.com>
2025-07-23 11:34:38 +0300

    Admin tarafında ve site üzerinde Öne Çıkan alanı dinamikleştirildi

---
+++ ApiProjeKampi.WebUI/Controllers/FeatureController.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using ApiProjeKampi.WebUI.Dtos.FeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ApiProjeKampi.WebUI.Controllers
+{
+    public class FeatureController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public FeatureController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> FeatureList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7100/api/Features");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData);
+                return View(value);
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateFeature(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7100/api/Features/GetFature?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<GetFeatureByIdDto>(jsonData);
+                return View(value);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateFeature(UpdateFeatureDto updateFeatureDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateFeatureDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7100/api/Features", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("FeatureList");
+            }
+            return View();
+        }
+
+    }
+}
--- ApiProjeKampi.WebUI/ViewComponents/_FeatureDefaultComponentPartial.cs
+++ ApiProjeKampi.WebUI/ViewComponents/_FeatureDefaultComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiProjeKampi.WebUI.Dtos.FeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ApiProjeKampi.WebUI.ViewComponents
 {
     public class _FeatureDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FeatureDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7100/api/Features");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData);
+                return View(value);
+            }
             return View();
         }
     }
